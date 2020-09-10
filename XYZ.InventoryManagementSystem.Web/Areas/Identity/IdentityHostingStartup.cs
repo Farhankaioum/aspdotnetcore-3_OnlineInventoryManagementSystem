08e8ef57b03c1d5d2d2768b4e4373a286ceb3f1f
Farhan Kaioum <km1472233@gmail.com>
2020-09-10 12:43:12 -0700

    Identity template added

---
+++ XYZ.InventoryManagementSystem.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using XYZ.InventoryManagementSystem.Web.Data;
+
+[assembly: HostingStartup(typeof(XYZ.InventoryManagementSystem.Web.Areas.Identity.IdentityHostingStartup))]
+namespace XYZ.InventoryManagementSystem.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
