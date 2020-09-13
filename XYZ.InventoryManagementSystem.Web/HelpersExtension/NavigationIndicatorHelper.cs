6ce19b5f528c82cdcc5fa290dbe76f7ed9e375ea
Farhan Kaioum <km1472233@gmail.com>
2020-09-12 23:12:22 -0700

    active page NavigationIndicatorHelper Extension method added

---
+++ XYZ.InventoryManagementSystem.Web/HelpersExtension/NavigationIndicatorHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace XYZ.InventoryManagementSystem.Web.HelpersExtension
+{
+    public static class NavigationIndicatorHelper
+    {
+        public static string MakeActiveClass(this IUrlHelper urlHelper, string controller, string action)
+        {
+            try
+            {
+                string result = "active";
+                string controllerName = urlHelper.ActionContext.RouteData.Values["controller"].ToString();
+                string methodName = urlHelper.ActionContext.RouteData.Values["action"].ToString();
+
+                if (string.IsNullOrEmpty(controllerName))
+                {
+                    return null;
+                }
+                if(controllerName.Equals(controller, StringComparison.OrdinalIgnoreCase) &&
+                    methodName.Equals(action, StringComparison.OrdinalIgnoreCase))
+                {
+                    return result;
+                }
+
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
